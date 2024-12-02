ec173b358f433a5dd097b50ae14f28cbeb6eca79
RAWANIO <rawanalhussaini77@gmail.com>
2024-12-02 18:41:17 +0300

    update

--- data.js
+++ data.js
@@ -1,26 +1,19 @@
-const url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        if (data && data.results) {
-            
-            const table = document.createElement('table');
-            table.classList.add('table', 'table-bordered'); 
 
-            
-            const thead = document.createElement('thead');
-            thead.innerHTML = `
-                <tr>
-                    <th>Year</th>
-                    <th>Semester</th>
-                    <th>The Programs</th>
-                    <th>Nationality</th>
-                    <th>Colleges</th>
-                    <th>Number of students</th>
-                </tr>
-            `;
-            table.appendChild(thead);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
            
             const tbody = document.createElement('tbody');
